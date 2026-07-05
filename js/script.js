d87cf854194c3a3d9ca243162812039fcb1bc9f3
Youssef Hawary <youssefelhawary012@gmail.com>
2026-07-05 08:48:58 +0300

    adding localStorage

--- js/script.js
+++ js/script.js
@@ -1,20 +1,22 @@
-let taskList = [] ;
+const taskInput = document.querySelector ( '#task' ) ;
+let taskList = JSON.parse ( localStorage.getItem ( 'taskList' ) ) || [] ;
 
 function add (  ) {
-    let task = document.querySelector ( '#task' ).value ;
-    
+    let task = taskInput.value.trim (  ) ;
+
     if ( task === '' )
-        alert ( 'Please enter your task!' ) ;
+        alert ( 'Add task please!' ) ; 
 
     else {
-        allTasks = {
-            task : task
+        const allTasks = {
+            task 
         } ;
 
         taskList.push ( allTasks ) ;
+        localStorage.setItem ( 'taskList' , JSON.stringify ( taskList ) ) ;
         displayTasks (  ) ;
         displayCount (  ) ;
-        document.querySelector ( '#task' ) = '' ;
+        taskInput.value = '' ;
     }
 }
 
@@ -22,7 +24,7 @@ function displayTasks (  ) {
     let output = '' ;
 
     for ( let i = 0 ; i < taskList.length ; i++ ) {
-        output += `<p style="padding-block : 1rem ;">Task${ i + 1 }: ${ taskList [ i ].task } <button class="btn2" onclick="deleteTasks (  )">Delete</button></p>` ;
+        output += `<p style="padding-block : 1rem ;">Task${ i + 1 }: ${ taskList [ i ].task }                          <button class="btn2"  onclick="deleteTasks ( ${ i } )">Delete</button></p>` ;
     }
 
     document.querySelector ( '#output' ).innerHTML = output ;
@@ -30,32 +32,46 @@ function displayTasks (  ) {
 
 function displayCount (  ) {
     let count = `<p class="total">Total Tasks: ${ taskList.length }</p>
-                        <p>Completed Tasks: ${ deleteCount }</p>` ;
+                <p>Completed Tasks: ${ deleteCount }</p>` ;
 
     document.querySelector ( '#countTasks' ).innerHTML = count ;
 }
 
-let deleteCount = 0 ;
+let deleteCount = Number ( localStorage.getItem ( 'deleteCount' ) ) || 0 ;
 
 function deleteTasks ( index ) {
-    taskList.splice ( index , 1 ) ;
-    deleteCount++ ;
-    displayTasks (  ) ;
-    displayCount (  ) ;
+    if ( confirm ( 'Are you sure to delete this task ?' ) ) {
+        taskList.splice ( index , 1 ) ;
+        localStorage.setItem ( 'taskList' , JSON.stringify ( taskList ) ) ;
+        deleteCount++ ;
+        localStorage.setItem ( 'deleteCount' , deleteCount ) ;
+        displayTasks (  ) ;
+        displayCount (  ) ;
+    }
 }
 
 const dark = document.querySelector ( '.mode' ) ;
-const icon = document.querySelector('.mode i');
+const icon = document.querySelector ( '.mode i' ) ;
+
 dark.onclick = function (  ) {
     document.body.classList.toggle ( 'dark' ) ;
 
     if ( document.body.classList.contains ( 'dark' ) ) {
-        icon.classList.remove ( 'fa-sun' ) ;
-        icon.classList.add ( 'fa-moon' ) ;
+        localStorage.setItem ( 'theme' , 'dark' ) ;
     }
 
     else {
-        icon.classList.remove ( 'fa-moon' ) ;
-        icon.classList.add ( 'fa-sun' ) ;
+        localStorage.setItem ( 'theme' , 'light' )
+    }
+
+    const theme = localStorage.getItem ( 'theme' ) ;
+
+    if ( theme === 'dark' ) {
+        document.body.classList.add ( 'dark' ) ;
+        icon.classList.remove ( 'fa-sun' ) ;
+        icon.classList.add ( 'fa-moon' ) ;
     }
 }
+
+displayTasks (  ) ;
+displayCount (  ) ;
